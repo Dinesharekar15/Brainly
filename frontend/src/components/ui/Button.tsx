8d81ade93dc3a6829d7d8553c6b1969a0e18ee71
dinesh <arekardinesh685@gmail.com>
2025-04-20 01:16:47 +0530

    Button Component

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,16 +1,36 @@
+import { ReactElement } from "react"
+
+
+
+
+type varianttpe="primary"|"secondary"
+
 interface ButttonProps{
-    variant:"primary"|"secondary",
-    starticon?:any,
-    endicon?:any,
+    variant:varianttpe,
+    starticon?:ReactElement,
+    endicon?:ReactElement,
     size:"sm"|"md"|"lg",
     text:string,
-    onClick:()=>void
+    onClick?:()=>void
 }
 
+const variantStyle={
+    "primary":"bg-purple-600 text-white",
+    "secondary":"bg-purple-400 text-purple-600"
+}
+
+const sizeStyle={
+    "sm":"py-1 px-2",
+    "md":"py-2 px-3",
+    "lg":"py-3 px-5"
+}
+
+const extrastyle="rounded-md flex"
 export const Button=(props:ButttonProps)=>{
     return (
-        <button>
-        
+        <button className={`${variantStyle[props.variant]} ${extrastyle} ${sizeStyle[props.size]}`}>
+            {props.starticon?<div className="pr-2">{props.starticon}</div> :null}
+        {props.text}
         </button>
     )
 }
